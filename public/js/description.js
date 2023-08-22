6f4641814a5a7c46405e2713d30569bc209bb42d
Aissam-salman <nasawi.lmj@gmail.com>
2023-08-22 09:57:54 +0200

    animation add

--- public/js/description.js
+++ public/js/description.js
@@ -1,9 +1,9 @@
 const modalContainer = document.querySelector('.container_modal');
 const triggers = document.querySelectorAll('.triggers');
+const overlay = document.querySelector('.overlay');
 
 triggers.forEach((trigger) => trigger.addEventListener('click', toggleModal));
 
 function toggleModal() {
     modalContainer.classList.toggle('active');
 }
-
